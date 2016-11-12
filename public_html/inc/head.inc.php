399d1f67a0caaaaaf4918d4b947361c5a2e6b1c6
Dragan Seatovic <dragan.seatovic@ticketmaster.com>
2016-11-12 20:43:05 +0100

    different images loaded on different screen sizes

--- public_html/inc/head.inc.php
+++ public_html/inc/head.inc.php
@@ -11,19 +11,19 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-                <img src="slike/<?=$activePage?>/slika1.jpg" alt="nesto">
+                <img src="slike/<?=$activePage?>/slika1-m.jpg" srcset="slike/<?=$activePage?>/slika1-m.jpg 1000w, slike/<?=$activePage?>/slika1-v.jpg 2000w">
             </div>
 
             <div class="item">
-                <img src="slike/<?=$activePage?>/slika2.jpg" alt="novo">
+                <img src="slike/<?=$activePage?>/slika2-m.jpg" srcset="slike/<?=$activePage?>/slika2-m.jpg 1000w, slike/<?=$activePage?>/slika2-v.jpg 2000w">
             </div>
 
             <div class="item">
-                <img src="slike/<?=$activePage?>/slika3.jpg" alt="nesto">
+                <img src="slike/<?=$activePage?>/slika3-m.jpg" srcset="slike/<?=$activePage?>/slika3-m.jpg 1000w, slike/<?=$activePage?>/slika3-v.jpg 2000w">
             </div>
 
             <div class="item">
-                <img src="slike/<?=$activePage?>/slika4.jpg" alt="divlje">
+                <img src="slike/<?=$activePage?>/slika4-m.jpg" srcset="slike/<?=$activePage?>/slika4-m.jpg 1000w, slike/<?=$activePage?>/slika4-v.jpg 2000w">
             </div>
         </div>
     </div>
